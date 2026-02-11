c97d6a8b845b7036a1e86774838a9cca3e07b1e1
9-uh <xak2wm@virginia.edu>
2026-02-10 23:32:50 -0500

    mywork/lab3 finished

--- mywork/lab3/analyze-moby-dick.sh
+++ mywork/lab3/analyze-moby-dick.sh
@@ -5,9 +5,9 @@ OUTPUT=$2
 
 curl https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt > mobydick.txt
 
-OCCURRENCES = $(grep -o "$SEARCH_PATTERN" mobydick.txt | wc -l)
+OCCURRENCES=$(grep -o "$SEARCH_PATTERN" mobydick.txt | wc -l)
 
-echo "The search pattern $SEARCH_PATTERN was found $OCCURRENCES times(s)." > $OUTPUT
+echo "The search pattern $SEARCH_PATTERN was found $OCCURRENCES times(s)." > "$OUTPUT"
 
 
 

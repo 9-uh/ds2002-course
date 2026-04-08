b4d708308b88c334d1a9043e9fa11ef43bf0a33e
Nina <xak2wm@virginia.edu>
2026-04-07 22:55:44 -0400

    Complete Lab 10: Containers (Docker on EC2 and Apptainer job array)

---
+++ mywork/lab10/add.sql
@@ -0,0 +1,8 @@
+USE xak2wm_db;
+
+INSERT INTO artists (name, home_country) VALUES ('Joe Diffie', 'USA');
+INSERT INTO artists (name, home_country) VALUES ('Dylan Cotrone', 'USA');
+
+INSERT INTO songs (song_name, genre, artist_id) VALUES ('Pickup Man', 'Country', 11);
+INSERT INTO songs (song_name, genre, artist_id) VALUES ('NO DEAL', 'Alternative', 12);
+INSERT INTO songs (song_name, genre, artist_id) VALUES ('Dog House', 'Alternative', 12);
---
+++ mywork/lab10/jokes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=ds2002
+#SBATCH --partition=standard
+#SBATCH --job-name=lolcow
+#SBATCH --output=lolcow_%j_%a.out
+#SBATCH --error=lolcow_%j_%a.err
+#SBATCH --time=00:01:00
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-10
+
+module load apptainer
+
+apptainer run ~/lolcow-latest.sif
